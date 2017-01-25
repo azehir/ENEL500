f2c8b2249fc3e599c8ba2bf23f5820484f22f799
altan <altan@DESKTOP-1FM2O85>
2017-01-25 14:00:13 -0700

    Threading Example Added / Thread Class removed as C# handles this sort of stuff for us

--- Main1/Main1/ExecFunc.cs
+++ Main1/Main1/ExecFunc.cs
@@ -3,20 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Main1
 {
     class ExecFunc
     {
-        static void Main(string[] args)
+
+        public static void ThreadProc()
         {
-
-
-
-
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                // Yield the rest of the time slice.
+                Thread.Sleep(0);
+            }
         }
 
+        static void Main()
+        {
+
+            Console.WriteLine("Main thread: Start a second thread.");
+            // The constructor for the Thread class requires a ThreadStart 
+            // delegate that represents the method to be executed on the 
+            // thread.  C# simplifies the creation of this delegate.
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+
+            // Start ThreadProc
+            t.Start();
+            //Thread.Sleep(1);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            Console.WriteLine("Main thread: Call Join(), to wait until ThreadProc ends.");
+            t.Join();
+            Console.WriteLine("Main thread: ThreadProc.Join has returned.  Press Enter to end program.");
+            Console.ReadLine();
+        }
+
+
     }
+
+    
 
 
 }
