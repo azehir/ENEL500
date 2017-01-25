5baccb20c71412a71b15f357222421415d2f822f
James Stephenson <james.o.steph@gmail.com>
2017-01-25 12:44:32 -0700

    Linked List/Node Changes

--- Main1/Main1/LinkedList.cs
+++ Main1/Main1/LinkedList.cs
@@ -8,7 +8,56 @@ namespace Main1
 {
     class LinkedList
     {
+        public class Node
+        {
+            public Node NodeNext;
+            private ulong u32LocTag;
+            private ulong u32TempData;
+            private uint u16xCoord;
+            private uint u16yCoord;
 
+            public Node()
+            {
+                NodeNext = null;
+                u32LocTag = 0;
+                u32TempData = 0;
+                u16xCoord = 0;
+                u16yCoord = 0;
+            }
+            public Node(ulong LocTag, ulong TempData)
+            {
+                NodeNext = null;
+                u32LocTag = LocTag;
+                u32TempData = TempData;
+                u16xCoord = 0;
+                u16yCoord = 0;
+            }
+
+            public void AddNode ()
+            {
+
+            }
+
+            public void RemoveNode(ulong TagID)
+            {
+
+            }
+
+            public void EditTemp_Node(ulong TagID, ulong TempData)
+            {
+
+            }
+
+            public void EditXYCoords_Node(int xCoord, int yCoord)
+            {
+
+            }
+
+            public void EditTag_Node(ulong TagID)
+            {
+
+            }
+        }
 
     }
 
