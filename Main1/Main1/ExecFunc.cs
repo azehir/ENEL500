fa283131cd5e214a2b443a5428385023739138f2
James Stephenson <james.o.steph@gmail.com>
2017-02-01 13:49:13 -0700

    Testing Image Proc

--- Main1/Main1/ExecFunc.cs
+++ Main1/Main1/ExecFunc.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
 
 namespace Main1
 {
     class ExecFunc
     {
-        Image newImage = new Image.FromFile("Back View Transformer Schematic.png");
+        
 
 
 
@@ -36,7 +38,21 @@ namespace Main1
 
         static void Main()
         {
-
+            using (var src = new Bitmap("c:/temp/trans.png"))
+            using (var bmp = new Bitmap(100, 100, PixelFormat.Format32bppPArgb))
+            using (var gr = Graphics.FromImage(bmp))
+            {
+                gr.Clear(Color.Black);
+                gr.DrawImage(src, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                bmp.Save("c:/temp/result.png", ImageFormat.Png);
+            }
+            //Bitmap image1 = new Bitmap("C:/Users/Laptop/Source/Repos/ENEL500/Main1/Back View Transformer Schematic.png");
+            //Bitmap image2 = new Bitmap("C:/Users/Laptop/Source/Repos/ENEL500/Main1/Side View Transformer Schematic.png");
+            //var bmp = new Bitmap(image1.Width, image1.Height, PixelFormat.Format32bppPArgb);
+            //var gr = Graphics.FromImage(image1);
+            //gr.Clear(Color.White);
+            //gr.DrawImage(image1, new Rectangle(0, 0, bmp.Width, bmp.Height));
+            //bmp.Save("C:/temp/result.png", ImageFormat.Png);
             //Console.WriteLine("Main thread: Start a second thread.");
             //// The constructor for the Thread class requires a ThreadStart 
             //// delegate that represents the method to be executed on the 
