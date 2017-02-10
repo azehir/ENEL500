f8e86e131be021403fd3b20a0fe70f94f6385e37
James Stephenson <james.o.steph@gmail.com>
2017-02-10 13:03:26 -0700

    C++ code for bluetooth comms

--- Main1/Main1/ImgProc.cs
+++ Main1/Main1/ImgProc.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
 
 namespace Main1
 {
     class ImgProc
     {
-
+        /*
+         * Load 
+         * 
+         * 
+         * 
+         * 
+         *
+         */ 
 
     }
 
