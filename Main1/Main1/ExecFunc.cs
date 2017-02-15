fc201b4b65233cab3776c9e0b000a63153672534
James Stephenson <james.o.steph@gmail.com>
2017-02-15 10:14:31 -0700

    Started Working on ImgProc need to talk with Altan on Plans for the Future

--- Main1/Main1/ExecFunc.cs
+++ Main1/Main1/ExecFunc.cs
@@ -26,23 +26,10 @@ namespace Main1
 
         static void Main()
         {
-            using (var src = new Bitmap("c:/temp/trans.png"))
-            using (var bmp = new Bitmap(100, 100, PixelFormat.Format32bppPArgb))
-            using (var gr = Graphics.FromImage(bmp))
-            {
-                gr.Clear(Color.Black);
-                gr.DrawImage(src, new Rectangle(0, 0, bmp.Width, bmp.Height));
-                bmp.Save("c:/temp/result.png", ImageFormat.Png);
-            }
 
 
-            //Bitmap image1 = new Bitmap("C:/Users/Laptop/Source/Repos/ENEL500/Main1/Back View Transformer Schematic.png");
-            //Bitmap image2 = new Bitmap("C:/Users/Laptop/Source/Repos/ENEL500/Main1/Side View Transformer Schematic.png");
-            //var bmp = new Bitmap(image1.Width, image1.Height, PixelFormat.Format32bppPArgb);
-            //var gr = Graphics.FromImage(image1);
-            //gr.Clear(Color.White);
-            //gr.DrawImage(image1, new Rectangle(0, 0, bmp.Width, bmp.Height));
-            //bmp.Save("C:/temp/result.png", ImageFormat.Png);
+
+           
             //Console.WriteLine("Main thread: Start a second thread.");
             //// The constructor for the Thread class requires a ThreadStart 
             //// delegate that represents the method to be executed on the 
--- Main1/Main1/ImgProc.cs
+++ Main1/Main1/ImgProc.cs
@@ -25,13 +25,75 @@ namespace Main1
          *      Plot Temp Data at Given pixel location
          *      Return New image with temp data plotted
          *      
-         *  Overlay Images
-         *      Recieve two images 
+         *  WE DONT NEED TO OVERLAY WE JUST NEED TO WRITE TEXT TO PICTURE. I AM SO STUPID
          * 
+         *  GUI might be a really good idea so that they can visually see the new mapped image immediately vs. having to open new file
          * 
          * 
          *
-         */ 
+         */
+        private string StrBitmapPath;
+        private Bitmap currentBitmap;
+        private Bitmap bitmapBeforeProcessing;
+       
+        public ImgProc()    {   }
+        public Bitmap CurrentBitmap
+        {
+            get
+            {
+                if (currentBitmap == null)
+                    currentBitmap = new Bitmap(1, 1);
+                return currentBitmap;
+            }
+            set { currentBitmap = value; }
+        }
+
+        public Bitmap BitmapBeforeProcessing
+        {
+            get { return bitmapBeforeProcessing; }
+            set { bitmapBeforeProcessing = value; }
+        }
+
+        public string BitmapPath
+        {
+            get { return StrBitmapPath; }
+            set { StrBitmapPath = value; }
+        }
+
+        public void ResetBitmap()
+        {
+            if(currentBitmap != null && bitmapBeforeProcessing != null)
+            {
+                Bitmap temp = (Bitmap)currentBitmap.Clone();
+                currentBitmap = (Bitmap)bitmapBeforeProcessing.Clone();
+                bitmapBeforeProcessing = (Bitmap)temp.Clone();
+            }
+        }
+
+        public void SaveBitmap(string saveFilePath)
+        {
+            StrBitmapPath = saveFilePath;
+            if (System.IO.File.Exists(saveFilePath))
+                System.IO.File.Delete(saveFilePath);
+            currentBitmap.Save(saveFilePath);
+        }
+
+        public void ClearImage()
+        {
+            currentBitmap = new Bitmap(1, 1);
+        }
+
+        public void RestorePrevious()
+        {
+            bitmapBeforeProcessing = currentBitmap;
+        }
+
+        //Continue more with Altan
+
+
+
+
+
 
     }
 
